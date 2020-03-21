cb4d9a7b18e76a0186f138847f2dddb7a7421084
junya <jun.okb.115@gmail.com>
2020-03-21 16:48:16 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import VueMaterial from 'vue-material'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 new Vue({
   router,
+  vuetify,
+  VueMaterial,
   render: h => h(App)
 }).$mount('#app')
