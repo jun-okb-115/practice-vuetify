55d5fcd352a44a617de5f20f9fc79792069877a7
jun-okb-115 <jun.okb.115@gmail.com>
2020-05-22 19:39:05 +0900

    [add] 15 dammy projects data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
-    <v-container class="my-10 grey lighten-2">
-      <v-card flat class="px-3 blue-grey lighten-5">
-        <v-row>
+    <v-container class="my-10">
+      <v-card
+        flat
+        class="px-3 blue-grey lighten-5"
+        v-for="project in projects"
+        :key="project.title">
+        <v-row :class="`pa-3 project ${ project.status }`">
           <v-col cols="12" md="6">
             <div class="caption grey--text">Project Title</div>
-            <div>Create a new website</div>
+            <div>{{ project.title }}</div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
             <div class="caption grey--text">Person</div>
-            <div>The Net samurai</div>
+            <div>{{ project.person }}</div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
             <div class="caption grey--text">Due by</div>
-            <div>20th May 2020</div>
+            <div>{{ project.due }}</div>
           </v-col>
           <v-col cols="2" sm="4" md="2">
             <div class="caption grey--text">Status</div>
-            <div>Ongoing</div>
+            <div>{{ project.status}}</div>
           </v-col>
         </v-row>
+        <v-divider></v-divider>
       </v-card>
     </v-container>
 
@@ -30,5 +35,27 @@
 <script>
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  },
 }
 </script>
+
+<style scoped>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+</style>
